b8c805bb74839f181c94db29ce65bacedf17ef0c
Eugene Lazutkin <eugene@lazutkin.com>
2013-07-30 22:18:31 -0500

    JSON-compliant string parsing, made stand-alone tokens for non-trivial entities.

--- tests/json.js
+++ tests/json.js
@@ -4,7 +4,14 @@
 
 	var rule = Grammar.rule, any = Grammar.any, maybe = Grammar.maybe, repeat = Grammar.repeat;
 
-	var ws = {id: "ws", pattern: /\s+/}
+	var ws = {id: "ws", pattern: /\s+/},
+		// numeric tokens
+		nonZero      = {id: "nonZero",      pattern: /[1-9]/},
+		exponent     = {id: "exponent",     pattern: /[eE]/},
+		numericChunk = {id: "numericChunk", pattern: /\d{1,256}/},
+		// string tokens
+		plainChunk   = {id: "plainChunk",   pattern: /[^\"\\]{1,256}/},
+		escapedChars = {id: "escapedChars", pattern: /\\(?:[bfnrt\"\\]|u[0-9a-fA-F]{4})/};
 
 	var json = new Grammar();
 
@@ -15,10 +22,10 @@
 	json.addRule("object", ["{", maybe(rule("pair"), repeat(",", rule("pair"))), "}"]);
 	json.addRule("pair",   [rule("string"), ":", rule("value")]);
 	json.addRule("array",  ["[", maybe(rule("value"), repeat(",", rule("value"))), "]"]);
-	json.addRule("string", ["\"", repeat(any(/[^\"\\]+/, /\\./)), "\""]);
-	json.addRule("number", [any("0", [/[1-9]/, repeat(/\d+/)]),
-		maybe(".", repeat(/\d+/)),
-		maybe(/[eE]/, maybe(maybe(any("-", "+")), repeat(/\d+/)))
+	json.addRule("string", ["\"", repeat(any(plainChunk, escapedChars)), "\""]);
+	json.addRule("number", [any("0", [nonZero, repeat(numericChunk)]),
+		maybe(".", repeat(numericChunk)),
+		maybe(exponent, maybe(maybe(any("-", "+")), repeat(numericChunk)))
 	]);
 
 	json.generate();
