dfb45fa1d8c28c17f7f191cb411db626f040baff
Eugene Lazutkin <eugene@lazutkin.com>
2013-08-11 23:44:14 -0500

    Compressed the sample to save some space.

--- tests/test_jsonsc.js
+++ tests/test_jsonsc.js
@@ -4,7 +4,7 @@ var Parser  = require("../topDown/Parser");
 
 var json = require("./json");
 
-var fs = require("fs"), path = require("path");
+var fs = require("fs"), path = require("path"), zlib = require("zlib");
 
 
 //optimize(json);
@@ -15,41 +15,46 @@ var scanner = new Scanner(),
 var CHUNK_SIZE = 1024;
 
 
-fs.readFile(path.resolve(__dirname, "sample.json"), "utf8", function(err, data){
+fs.readFile(path.resolve(__dirname, "sample.json.gz"), function(err, data){
 	if(err){
 		throw err;
 	}
+	zlib.gunzip(data, function(err, data){
+		if(err){
+			throw err;
+		}
 
-	function addChunk(){
-		var chunk = data.substring(0, CHUNK_SIZE);
-		data = data.substring(CHUNK_SIZE);
-		scanner.addBuffer(chunk, !data);
-	}
+		function addChunk(){
+			var chunk = data.slice(0, CHUNK_SIZE);
+			data = data.slice(CHUNK_SIZE);
+			scanner.addBuffer(chunk, !data.length);
+		}
 
-	function getToken(state){
+		function getToken(state){
+			for(;;){
+				var token = scanner.getToken(state);
+				if(token !== true){
+					// no need for the next chunk yet
+					break;
+				}
+				addChunk();
+			}
+			return token;
+		}
+
+		// let's loop over tokens
 		for(;;){
-			var token = scanner.getToken(state);
-			if(token !== true){
-				// no need for the next chunk yet
+			var expected = parser.getExpectedState();
+			if(!expected){
+				// we are done
 				break;
 			}
-			addChunk();
+			var token = getToken(expected);
+			parser.putToken(token, scanner);
 		}
-		return token;
-	}
 
-	// let's loop over tokens
-	for(;;){
-		var expected = parser.getExpectedState();
-		if(!expected){
-			// we are done
-			break;
+		if(!scanner.isFinished()){
+			throw Error("Scanner has some unprocessed symbols.");
 		}
-		var token = getToken(expected);
-		parser.putToken(token, scanner);
-	}
-
-	if(!scanner.isFinished()){
-		throw Error("Error: scanner has some unprocessed symbols.");
-	}
+	});
 });
