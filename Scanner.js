a1a203ec6f6ab8e9690e14b5a917f7a3bbda46c7
Eugene Lazutkin <eugene@lazutkin.com>
2014-12-11 14:49:00 -0600

    Added direct support of Buffer, and a manual (fast) new line detection.

--- Scanner.js
+++ Scanner.js
@@ -17,7 +17,7 @@
 		},
 
 		addBuffer: function(buffer, noMore){
-			this.buffer += buffer;
+			this.buffer += Buffer.isBuffer(buffer) ? buffer.toString() : buffer;
 			this.noMore = noMore;
 		},
 
@@ -29,7 +29,7 @@
 
 		// private workings
 
-		newLinePattern: /[\u000A\u2028\u2029]|\u000D\u000A|\u000D/g,
+		newLinePattern: null, // /[\u000A\u2028\u2029]|\u000D\u000A|\u000D/g,
 
 		getToken: function(state, peek){
 			var buffer = this.buffer;
@@ -57,13 +57,35 @@
 			token.pos   = this.pos;
 			// update line and position, if it was not a peek
 			if(!peek){
-				var self = this, rest = matched.length;
-				matched.replace(this.newLinePattern, function(match, offset){
-					rest = matched.length - match.length - offset;
-					++self.line;
-					self.pos = 1;
-					return "";
-				});
+				var rest = matched.length;
+				if(this.newLinePattern){
+					var self = this;
+					matched.replace(this.newLinePattern, function(match, offset){
+						rest = matched.length - match.length - offset;
+						++self.line;
+						self.pos = 1;
+						return "";
+					});
+					this.pos += rest;
+				}else{
+					for(var i = 0, n = rest; i < n; ++i){
+						switch(matched.charCodeAt(i)){
+							case 0x0A:
+							case 0x2028:
+							case 0x2029:
+								rest = 0;
+								break;
+							case 0x0D:
+								rest = i + 1 < n && matched.charCodeAt(i + 1) === 0x0A ? -1 : 0;
+								break;
+							default:
+								continue;
+						}
+						rest += n - i - 1;
+						++this.line;
+						this.pos = 1;
+					}
+				}
 				this.pos += rest;
 				this.buffer = buffer.substring(matched.length);
 			}
